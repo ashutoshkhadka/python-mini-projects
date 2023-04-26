15653d0df3f11e04bb44d75e937cdff46e470042
Ashutosh Khadka <khadkashutosh@outlook.com>
2023-04-26 16:41:17 -0700

    guessing game impl done

--- higher_or_lower/main.py
+++ higher_or_lower/main.py
@@ -1,4 +1,4 @@
-import random
+import random, os
 from art import vs, logo
 from data import data
 
@@ -27,26 +27,46 @@ def has_more_followers(choice1, choice2):
     return choice1["follower_count"] > choice2["follower_count"]
 
 
-print(logo)
+def correct_answer(score):
+    score += 1
+    print("Correct")
+    os.system("clear")
+    print(logo)
+    print(f"You're right 🥳. Current score: {score}")
+    return score
+
+
+def wrong_answer(score):
+    os.system("clear")
+    print(logo)
+    print(f"Sorry, that's wrong 😥. Your Final score is: {score}")
+
+
+def play_game():
+    print(logo)
+    score = 0
+    choice1 = retrieve_unique_data()
+    choice2 = retrieve_unique_data()
+
+    while True:
+        print_elem("A", choice1)
+        print(vs)
+        print_elem("B", choice2)
+
+        if input("Who's got more followers? A or B??\n").lower() == "a":
+            if has_more_followers(choice1, choice2):
+                score = correct_answer(score)
+                choice2 = retrieve_unique_data()
+            else:
+                wrong_answer(score)
+                break
+        else:
+            if has_more_followers(choice2, choice1):
+                score = correct_answer(score)
+                choice1 = retrieve_unique_data()
+            else:
+                wrong_answer(score)
+                break
+
 used_data = []
-
-choice1 = retrieve_unique_data()
-choice2 = retrieve_unique_data()
-
-print_elem("A", choice1)
-print("vs")
-print_elem("B", choice2)
-
-if input("Who's got more followers? A or B??\n").lower() == "a":
-    if has_more_followers(choice1, choice2):
-        print("Correct")
-    else:
-        print("Sorry, you lose")
-else:
-    if has_more_followers(choice2, choice1):
-        print("Correct")
-    else:
-        print("Sorry, you lose")
-
-print(f"{choice1['name']}: {choice1['follower_count']}")
-print(f"{choice2['name']}: {choice2['follower_count']}")
+play_game()
