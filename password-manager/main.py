accfaa828fc2f4d20ca81a67bf6dc2fb84254772
Ashutosh Khadka <khadkashutosh@outlook.com>
2023-05-03 18:58:42 -0700

    exceptions integration

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,3 +1,4 @@
+import json
 from tkinter import *
 from tkinter import messagebox
 import random
@@ -28,15 +29,43 @@ def save_password():
     website = website_input.get()
     email = username_input.get()
     password = password_input.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showerror(title="Missing information", message="Please fill out the mandatory values")
     else:
-        with open("data.txt", "a") as f:
-            f.write(f"{website}|{email}|{password}\n")
-        website_input.delete(0, END)
-        password_input.delete(0, END)
+        try:
+            with open("data.json", "r") as f:
+                data = json.load(f)
+                data.update(new_data)
+        except FileNotFoundError:
+            write_data = new_data
+        else:
+            write_data = data
+        finally:
+            with open("data.json", "w") as f:
+                json.dump(write_data, f, indent=4)
+    website_input.delete(0, END)
+    password_input.delete(0, END)
 
+
+def search_password():
+    try:
+        with open("data.json", "r") as f:
+            data = json.load(f)
+            website = website_input.get()
+            credentials = data[website]
+            messagebox.showinfo(title="Credentials",
+                                message=f"username: {credentials['email']}\n password: {credentials['password']}")
+    except FileNotFoundError:
+        messagebox.showerror(title="No data found", message="File does not exist")
+    except KeyError:
+        messagebox.showerror(title="No data found", message=f"No credentials found for the website: {website}")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -54,8 +83,8 @@ canvas.grid(column=1, row=0)
 website_label = Label(text="Website")
 website_label.grid(row=1, column=0)
 
-website_input = Entry(width=35)
-website_input.grid(row=1, column=1, columnspan=2, sticky=W)
+website_input = Entry(width=21)
+website_input.grid(row=1, column=1)
 website_input.focus()
 
 username_label = Label(text="Username/Email")
@@ -73,6 +102,9 @@ password_input.grid(row=3, column=1, sticky=W)
 
 generate_button = Button(text="Generate", width=9, command=generate_password)
 generate_button.grid(row=3, column=2, sticky=N)
+
+search_button = Button(text="Search", command=search_password, width=9)
+search_button.grid(row=1, column=2)
 
 add_button = Button(text="Add", width=32, command=save_password)
 add_button.grid(row=4, columnspan=2, column=1, sticky=W)
